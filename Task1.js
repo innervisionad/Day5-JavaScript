d0455b9fb16be59f31932e4f24dd4cbe7984506c
Adam Clements <aj.clements@live.co.uk>
2016-06-17 14:29:35 +0100

    Task 1

---
+++ Task1.js
@@ -0,0 +1,15 @@
+var event1 = document.getElementById("acceptString");
+event1.addEventListener('click', changeString, false);
+
+var string1 = document.getElementById("input1");
+
+
+function changeString() {
+    
+    var upString1 = string1.value.toLocaleUpperCase();
+    var lowString1 = string1.value.toLocaleLowerCase();
+    
+    document.getElementById("unmodString").innerHTML = string1.value;
+    document.getElementById("upcaseString").innerHTML = upString1;
+    document.getElementById("lowcaseString").innerHTML = lowString1;
+}
